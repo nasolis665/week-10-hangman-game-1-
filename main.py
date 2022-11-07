fbe9d0c29e05a68a96a440eed6726a4048233cd5
Nathan Solis <113551835+nasolis665@users.noreply.github.com>
2022-11-07 20:38:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# from methods_help import method_help
+# from functions import function
+# from returnStuff import returnS
+
+# method_help()
+# function()
+# returnS()
+from function_interactions import *
+
+sticks=["-","--","---","----"]
+my_mix=mix(sticks)
+
+try1=try_your_luck()
+print(try1)
+
+verify_my_try(my_mix, try1)
---
+++ methods_help.py
@@ -0,0 +1,58 @@
+def greetings(name):
+  print("hello "+ name)
+def method_help():
+  # pass
+
+  
+  dic={"key1": 100, "key2":200}
+  dic.popitem()
+########################################################################################################################
+  # Methods, Help & Documentation Practice #1
+  # Remove the characters to the left of our main text:
+  
+  # ,
+  
+  # :
+  
+  # %
+  
+  # _
+  
+  # #
+  
+  # Use the lstrip() method. Print the result to the screen:
+  
+  # ",:_#,,,,,,:::____##Total_ _Pyt%on,,,,,,::#"
+  
+  # Search the documentation for the requested method to learn how it works. You can use intermediate variables if you need them.
+  "Total Pyton"
+
+
+
+
+
+
+
+########################################################################################################################
+  # Methods, Help & Documentation Practice #2
+  # Add the element "orange" as the fourth element of the following list fruits, using the insert() method:
+  
+  fruits = ["mango", "banana", "cherry", "plum", "grapefruit"]
+  fruits.insert(3,"orange")
+  # Search the documentation for the requested method to know how it works.
+
+
+
+
+
+
+
+########################################################################################################################
+  #   Methods, Help & Documentation Practice #3
+  # Check if the sets below are isolated (that is, they have no elements in common), using the isdisjoint() method. Store this result in the isolated_sets variable:
+  
+  phone_brands={"Samsung", "Xiaomi", "Apple", "Huawei", "LG"}
+  tv_brands= {"Sony", "Philips", "Samsung", "LG"}
+  isolated_tvs = phone_brands.isdisjoint(tv_brands)
+  print(isolated_tvs)
+  # Search the documentation for the requested method to know how it works.
